93d25de3a139b66191b7ecfe3408a54df0aebbca
Emil Englesson <englesson.emil@gmail.com>
2025-03-23 21:15:35 +0100

    Updated fuzzing targets

--- fuzz/fuzz_targets/no_panic.rs
+++ fuzz/fuzz_targets/no_panic.rs
@@ -1,6 +1,12 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
 
+use wthor::{game_archive::GameArchive, records::Records};
+
 fuzz_target!(|data: &[u8]| {
-    let _result = wthor::parse(data);
+    let _result = wthor::parse::<GameArchive>(data);
+});
+
+fuzz_target!(|data: &[u8]| {
+    let _result = wthor::parse::<Records>(data);
 });
